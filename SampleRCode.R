66eaeff2caac97f29de9f984793d2bf681ee4950
ishaar6 <40862088+ishaar6@users.noreply.github.com>
2020-12-17 21:28:15 -0600

    Add files via upload

--- SampleRCode.R
+++ SampleRCode.R
@@ -6,7 +6,7 @@ install.packages("dplyr")
 library("dplyr")
 
 #sample code to determine the frequency of various X chromosome regions in CN-LOH 
-data_1 <- read_excel("cn_wgd_all_code4.xlsx")
+data_1 <- read_xlsx("cn_wgd_all_code4.xlsx")
 
 # filter for CN-LOH and X chromosomes only
 data_LOH <-filter(data_1, segment_cn == "Copy_neutral_LOH", chromosome == "X")
@@ -25,8 +25,10 @@ data_LOHfinal <- data_LOHsum %>% filter(Freq > 10)
 data_barplot <- data_LOHfinal %>% mutate( x_axis= str_c(start, end, sep="-"))
 
 #Add relevant title and x and y axis labels.
-final_plot=ggplot(data = data_barplot, aes(x_axis, Freq)) + geom_bar(stat= "identity", fill="lightblue")+
+final_plot <- ggplot(data = data_barplot, aes(x_axis, Freq)) + geom_bar(stat= "identity", fill="lightblue")+
 labs(title= "Frequency of CNLOH regions in X Chromosome From TCGA Data") + xlab("Base Pair Range in X Chromosome") + ylab("Frequency of Significant Region")
+
+final_plot
 
 ##I tried to provide here most of the things that would probably be done with surveys with data manipulation
 ##as most of my previous work was using R in the preliminary stages for data cleaning 
